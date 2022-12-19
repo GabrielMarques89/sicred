899d2632e64dd64c8557e10bbf318c0f7fe92049
GabrielMarques89 <marques.gabriel.1989@gmail.com>
2022-12-18 23:31:50 -0300

    TICKET-XXXX: Fluxo de trabalho de análise de performance funcional

--- src/test/java/design/boilerplate/springboot/service/performance/PerformanceSimulation.java
+++ src/test/java/design/boilerplate/springboot/service/performance/PerformanceSimulation.java
@@ -3,13 +3,11 @@ package design.boilerplate.springboot.service.performance;
 import static io.gatling.javaapi.core.CoreDsl.AllowList;
 import static io.gatling.javaapi.core.CoreDsl.DenyList;
 import static io.gatling.javaapi.core.CoreDsl.StringBody;
-import static io.gatling.javaapi.core.CoreDsl.arrayFeeder;
-import static io.gatling.javaapi.core.CoreDsl.csv;
 import static io.gatling.javaapi.core.CoreDsl.exec;
+import static io.gatling.javaapi.core.CoreDsl.feed;
 import static io.gatling.javaapi.core.CoreDsl.jsonPath;
-import static io.gatling.javaapi.core.CoreDsl.rampUsers;
-import static io.gatling.javaapi.core.CoreDsl.repeat;
 import static io.gatling.javaapi.core.CoreDsl.scenario;
+import static io.gatling.javaapi.core.OpenInjectionStep.atOnceUsers;
 import static io.gatling.javaapi.http.HttpDsl.http;
 import static io.gatling.javaapi.http.HttpDsl.status;
 import static util.DtoHelper.generateUserRegistrationRequest;
@@ -21,8 +19,6 @@ import design.boilerplate.springboot.model.dto.SessionDto;
 import design.boilerplate.springboot.model.dto.SessionOpenningDto;
 import design.boilerplate.springboot.model.dto.TopicRequest;
 import design.boilerplate.springboot.model.dto.UserRegistrationRequest;
-import design.boilerplate.springboot.model.dto.VoteRequest;
-import design.boilerplate.springboot.model.dto.VoteRequestDto;
 import io.gatling.javaapi.core.ChainBuilder;
 import io.gatling.javaapi.core.ScenarioBuilder;
 import io.gatling.javaapi.core.Simulation;
@@ -32,14 +28,16 @@ import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Random;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 import org.apache.commons.lang3.RandomStringUtils;
+import util.GeraCpfCnpj;
 
 public class PerformanceSimulation extends Simulation {
 
-  public static final String HTTP_LOCALHOST_8080 = "http://localhost:8080";
+  public static final String HOST = "http://localhost:8080";
   private static final Gson gson = new Gson();
   public static final String GZIP_DEFLATE = "gzip, deflate";
   public static final String ACCEPT_HEADER = "*/*";
@@ -49,15 +47,26 @@ public class PerformanceSimulation extends Simulation {
   public static final int HTTTP_CREATED = 201;
   public static final int HTTP_OK = 200;
   private static final Random random = new Random();
+  public static final String LOGIN_URL = "/login";
+  public static final String CREATE_TOPIC_URL = "/topic/v2";
+  public static final String CREATE_SESSION_URL = "/session/v2";
   private Long sessionId;
+  private Queue<UserRegistrationRequest> feederUsers;
+  private final GeraCpfCnpj cpfGenerator = new GeraCpfCnpj();
+  private final String sessionDate = LocalDateTime.now().minusMinutes(5).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
 
   public PerformanceSimulation() {
-    var prepareSessionScenario = prepareSession().injectOpen(rampUsers(3).during(15));
+//    var prepareSessionScenario = postUserAndLogin().injectOpen(rampUsers(100).during(15));
+    var prepareSessionScenario = prepareSession().injectOpen(atOnceUsers(2));
     setUp(prepareSessionScenario).protocols(httpProtocol());
   }
 
+  private ScenarioBuilder prepareSession() {
+    return scenario("creating_session").exec(createUser(), loginUser(), createTopic(), createSession());
+  }
+
   private HttpProtocolBuilder httpProtocol() {
-    return http.baseUrl(HTTP_LOCALHOST_8080)
+    return http.baseUrl(HOST)
         .inferHtmlResources(AllowList(), DenyList())
         .acceptHeader(ACCEPT_HEADER)
         .acceptEncodingHeader(GZIP_DEFLATE)
@@ -65,90 +74,68 @@ public class PerformanceSimulation extends Simulation {
         .userAgentHeader(AGENT_HEADER);
   }
 
-  private ChainBuilder createTopic(TopicRequest topic) {
-    return exec(http("create_topic").post("/topic/v2").header("Authorization", "Bearer #{token}")
-        .body(StringBody(gson.toJson(topic)))
+  private ChainBuilder createUser() {
+    return
+        feed(userFeed)
+            .exec(http("user_creation").post(USER_CREATION_URL).body(
+                    StringBody(session -> gson.toJson((UserRegistrationRequest) session.get("user"))))
+                .check(status().is(HTTTP_CREATED)))
+            .exec(session -> {
+              var user = (UserRegistrationRequest) session.get("user");
+              var loginRequest = gson.toJson(new LoginRequest(user.getUsername(), user.getPassword()));
+              return session.set("loginRequest", loginRequest);
+            });
+  }
+
+  private ChainBuilder loginUser() {
+    return exec(http("login").post(LOGIN_URL)
+                .body(StringBody(session -> session.get("loginRequest")))
+                .check(status().is(HTTP_OK))
+                .check(jsonPath("$.token").saveAs("token")));
+  }
+
+  private ChainBuilder createTopic() {
+    return
+        feed(topicFeeder)
+        .exec(http("create_topic").post(CREATE_TOPIC_URL).header("Authorization", "Bearer #{token}")
+        .body(StringBody(session -> gson.toJson(new TopicRequest(session.get("topic")))))
         .check(status().is(HTTTP_CREATED))
-        .check(jsonPath("$.id").saveAs("topicId"))
-    );
+        .check(jsonPath("$.id").saveAs("topicId")));
   }
 
-  private ChainBuilder loginUser(LoginRequest login) {
-    return exec(http("login").post("/login")
-        .body(StringBody(gson.toJson(login)))
-        .check(status().is(HTTP_OK))
-        .check(jsonPath("$.token").saveAs("token")));
+
+  private ChainBuilder createSession() {
+    return
+        exec(session -> {
+          var topic = session.getString("topicId");
+          var sessionRequest = gson.toJson(new SessionDto(topic, sessionDate));
+          return session.set("sessionRequest", sessionRequest);
+        })
+            .exec(http("create_session").post(CREATE_SESSION_URL)
+                .header("Authorization", "Bearer #{token}")
+                .body(StringBody(session -> session.get("sessionRequest")))
+                .asJson()
+                .check(status().is(HTTTP_CREATED)).check(jsonPath("$.id").saveAs("sessionId")));
   }
 
-  private ChainBuilder createSession(String begin) {
-    return exec(
-        http("create_session").post("/session/v2").header("Authorization", "Bearer #{token}")
-            .body(StringBody(gson.toJson(new SessionDto("${topicId}", begin)))).asJson()
-            .check(status().is(HTTTP_CREATED))
-            .check(jsonPath("$.id").saveAs("sessionId")));
-  }
   private ChainBuilder openSession(int duration) {
-    return exec(http("open_session").put("/session/begin").header("Authorization", "Bearer #{token}")
-        .body(StringBody(gson.toJson(new SessionOpenningDto("${sessionId}", 10))))
-        .check(status().is(HTTP_OK)));
+    return exec(
+        http("open_session").put("/session/begin").header("Authorization", "Bearer #{token}")
+            .body(StringBody(gson.toJson(new SessionOpenningDto("${sessionId}", 10))))
+            .check(status().is(HTTP_OK)));
   }
 
-  private ChainBuilder createUser(UserRegistrationRequest user) {
-    var chain = exec(http("user_creation").post(USER_CREATION_URL)
-        .body(StringBody(gson.toJson(user)))
-        .check(status().is(HTTTP_CREATED)))
-        .pause(5);
-    return chain;
-  }
+  Iterator<Map<String, Object>> userFeed = Stream.generate(
+      (Supplier<Map<String, Object>>) () -> Collections.singletonMap("user",
+          generateUserRegistrationRequest())).iterator();
 
-  private Iterator<Map<String, UserRegistrationRequest>> createFeeder() {
-    return Stream.generate((Supplier<Map<String, UserRegistrationRequest>>) () -> {
-          var generateUser = generateUserRegistrationRequest();
-          return Collections.singletonMap("user", generateUser);
-        }
-    ).iterator();
-  }
-
-  private ScenarioBuilder prepareSession() {
-
-    var user = generateUserRegistrationRequest();
-    var login = new LoginRequest(user.getUsername(), user.getPassword());
-    var topic = new TopicRequest("Topic of user " + user.getUsername());
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-    var begin = LocalDateTime.now().minusMinutes(5).format(formatter);
-
-    var vote = new VoteRequest();
-
-    return scenario("creating_session").exec(createUser(user), loginUser(login), createTopic(topic),
-        createSession(begin), openSession(15));
-  }
+  Iterator<Map<String, Object>> topicFeeder =
+      Stream.generate((Supplier<Map<String, Object>>) () -> {
+        String generatedTopicName = "Pauta Aleatória " + RandomStringUtils.randomAlphanumeric(20);
+        return Collections.singletonMap("topic", generatedTopicName);
+      }).iterator();
 
   private VoteResult randomVote() {
     return random.nextInt(2) == 0 ? VoteResult.YES : VoteResult.NO;
-  }
-
-  private ScenarioBuilder createUserAndVote() {
-    var user = generateUserRegistrationRequest();
-    var login = new LoginRequest(user.getUsername(), user.getPassword());
-    var vote = new VoteRequest(18L, randomVote());
-
-    return scenario("userCreationAndVote")
-        .exec(
-            http("user_creation")
-                .post(USER_CREATION_URL)
-                .body(StringBody(gson.toJson(user)))
-                .check(status().is(HTTTP_CREATED)))
-        .exec(
-            http("login")
-                .post("/login")
-                .body(StringBody(gson.toJson(login)))
-                .check(status().is(HTTP_OK))
-                .check(jsonPath("$.token").saveAs("token")))
-        .exec(
-            http("vote")
-                .post("/vote")
-                .body(StringBody(gson.toJson(new VoteRequestDto("#{sessionId}", randomVote()))))
-                .check(status().is(HTTP_OK))
-        );
   }
 }
