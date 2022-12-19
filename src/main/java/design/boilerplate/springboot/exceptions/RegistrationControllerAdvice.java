6f95b08f33b11bf9209d08b8544eaab8e12fb901
GabrielMarques89 <marques.gabriel.1989@gmail.com>
2022-12-19 15:30:25 -0300

    TICKET-XXXX: Correção de bug na validação de modelo de request

--- src/main/java/design/boilerplate/springboot/exceptions/RegistrationControllerAdvice.java
+++ src/main/java/design/boilerplate/springboot/exceptions/RegistrationControllerAdvice.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 
-@RestControllerAdvice(basePackageClasses = UserController.class)
+@RestControllerAdvice
 public class RegistrationControllerAdvice {
 
   @ExceptionHandler(RegistrationException.class)
--- src/main/java/design/boilerplate/springboot/model/dto/VoteRequest.java
+++ src/main/java/design/boilerplate/springboot/model/dto/VoteRequest.java
@@ -1,13 +1,16 @@
 package design.boilerplate.springboot.model.dto;
 
 import design.boilerplate.springboot.model.VoteResult;
+import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@ToString
 public class VoteRequest {
 
   public VoteRequest(Long session, VoteResult voteResult) {
@@ -15,6 +18,7 @@ public class VoteRequest {
     this.voteResult = voteResult;
   }
 
+  @NotNull(message = "{vote_session_not_empty}")
   private Long session;
   private VoteResult voteResult;
 }
